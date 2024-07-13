001d32cebbfa200eb595e37945c83d702dd14ad0
Shakib Sufian <142807525+programmersufian@users.noreply.github.com>
2024-07-13 08:32:29 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// const name = "0";
+
+// False
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truth
+// '0', ' ', []
+
+let name = 0;
+console.log(name);
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
